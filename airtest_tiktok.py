fd1162cd5f64a54318aaeb06acc631808eef2f0d
OwOY <63274030+OwOY@users.noreply.github.com>
2020-10-26 19:13:07 +0800

    Update airtest_tiktok.py

--- airtest_tiktok.py
+++ airtest_tiktok.py
@@ -7,6 +7,7 @@ import emoji
 import string
 import re
 import os
+from random import randint
 
 poco = AndroidUiautomationPoco(use_airtest_input=True, screenshot_each_action=False)
 auto_setup(__file__)
@@ -26,6 +27,7 @@ class filters:
         
         title = emoji.demojize(title, delimiters=(':',':'))
         title = re.sub('[’!"#$%&\'()*+,-./:;<=>?@，。?★、…【】《》？“”‘’！[\\]^_`{|}~\s]+', "", title)
+        title = re.sub(':\S+?:',' ',title)
         
         return title
     
@@ -57,32 +59,35 @@ def move_download_and_clean(title):
 
     
 def main():
-    
+    rest = randint(2, 5)
     poco(text="抖音").click()
     sleep(5)
 
     while True:
         try:
             try:
-                title = poco(name="com.ss.android.ugc.aweme:id/a94").get_text()
+                title = poco(name = "com.ss.android.ugc.aweme:id/a94").get_text()
             except:
-                title = poco(name="com.ss.android.ugc.aweme:id/title").get_text()
+                title = poco(name = "com.ss.android.ugc.aweme:id/title").get_text()
             title = filters.title_filter(title)
             print(f'======================{title}==========================')
+            #點擊分享按鈕
             poco("com.ss.android.ugc.aweme:id/g72").click()
             sleep(0.5)
+            #點擊下載
             poco("android.widget.LinearLayout").offspring("com.ss.android.ugc.aweme:id/c3").child("android.widget.LinearLayout")[1].offspring("com.ss.android.ugc.aweme:id/g64").click()
+            #確認是否下載完成
             while True:
                 try:
                     poco(name = "com.ss.android.ugc.aweme:id/e7f").click()
                 except:
                     break
-            sleep(2)
+            sleep(rest)
             move_download_and_clean(title)
         except:
             pass
         poco.swipe([0.5, 0.7], [0.5, 0.3], duration = 0.3)
-        sleep(2)
+        sleep(rest)
 
     
     
